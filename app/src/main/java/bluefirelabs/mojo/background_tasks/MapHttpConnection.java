6143a2f1950643732a78777aa7969ae206d5bc95
Reza Rajan <rezarajan@gmail.com>
2017-05-16 17:22:10 -0400

    Added the function to create a polyline on a Google Map fragment between two points. The underlying funtions to achieve this may also be used to determine the time to destination.

---
+++ app/src/main/java/bluefirelabs/mojo/background_tasks/MapHttpConnection.java
@@ -0,0 +1,46 @@
+package bluefirelabs.mojo.background_tasks;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by Reza Rajan on 2017-05-16.
+ */
+
+public class MapHttpConnection {
+    public String readUr(String mapsApiDirectionsUrl) throws IOException {
+        String data = "";
+        InputStream istream = null;
+        HttpURLConnection urlConnection = null;
+        try {
+            URL url = new URL(mapsApiDirectionsUrl);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.connect();
+            istream = urlConnection.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(istream));
+            StringBuffer sb = new StringBuffer();
+            String line ="";
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+            data = sb.toString();
+            br.close();
+
+
+        }
+        catch (Exception e) {
+            Log.d("Exception", e.toString());
+        } finally {
+            istream.close();
+            urlConnection.disconnect();
+        }
+        return data;
+
+    }
+}
