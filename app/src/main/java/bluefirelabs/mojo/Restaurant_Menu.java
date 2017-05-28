0c7415b8842c56f63965bc77ebcb1a5c85999b03
Reza Rajan <rezarajan@gmail.com>
2017-05-28 07:24:40 -0400

    Fixed the onClick bug redirecting to the wrong activity on the MainHub

--- app/src/main/java/bluefirelabs/mojo/Restaurant_Menu.java
+++ app/src/main/java/bluefirelabs/mojo/Restaurant_Menu.java
@@ -1,63 +1,18 @@
 package bluefirelabs.mojo;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import com.firebase.ui.database.FirebaseRecyclerAdapter;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import bluefirelabs.mojo.handlers.RecyclerAdapter_Menu;
 
 public class Restaurant_Menu extends AppCompatActivity {
 
-    public static class RecyclerViewHolder extends RecyclerView.ViewHolder {
-
-        private Context context;
-        public TextView itemTitle;
-        public TextView itemDescription;
-        public ImageView itemIcon;
-
-        public RecyclerViewHolder(View itemView) {
-            super(itemView);
-
-            itemIcon = (ImageView) itemView.findViewById(R.id.item_icon);
-            itemTitle = (TextView) itemView.findViewById(R.id.item_title);
-            itemDescription = (TextView) itemView.findViewById(R.id.item_description);
-            context = itemView.getContext();
-
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    int position = getAdapterPosition();
-
-                    Snackbar.make(v, "Click detected on item " + position,
-                            Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
-
-                    final Intent intent;
-
-                    intent = new Intent(context, Restaurant_Menu.class);
-                    context.startActivity(intent);
-
-                }
-            });
-        }
-    }
-
-
-    public static final String RESTAURANT = "listing";
-    private DatabaseReference mFirebaseDatabaseReference;
-    private FirebaseRecyclerAdapter<Restaurant_List, RecyclerViewHolder> mFirebaseAdapter;
-
-    private RecyclerView mRestaurantRecyclerView;
-    private LinearLayoutManager mLinearLayoutManager;
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    RecyclerView.Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,40 +21,18 @@ public class Restaurant_Menu extends AppCompatActivity {
         //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         //toolbar.setTitle("Menu");
 
-        mRestaurantRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        mLinearLayoutManager = new LinearLayoutManager(this);
-        mLinearLayoutManager.setStackFromEnd(true);
+        CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout)findViewById(R.id.collapsingToolbar);
+        collapsingToolbar.setTitle("Restaurant");
 
-        mFirebaseDatabaseReference = FirebaseDatabase.getInstance().getReference();
-        mFirebaseAdapter = new FirebaseRecyclerAdapter<Restaurant_List, RecyclerViewHolder>(
-                Restaurant_List.class,
-                R.layout.card_layout,
-                RecyclerViewHolder.class,
-                mFirebaseDatabaseReference.child(RESTAURANT)
-        ) {
-            @Override
-            protected void populateViewHolder(RecyclerViewHolder viewHolder, Restaurant_List model, int position) {
-                viewHolder.itemDescription.setText(model.getDescription());
-                viewHolder.itemTitle.setText(model.getRestaurant());
-                viewHolder.itemIcon.setImageResource(R.drawable.restaurant_icon);
-            }
-        };
 
-        mFirebaseAdapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver(){
-            @Override
-            public void onItemRangeInserted(int positionStart, int itemCount) {
-                super.onItemRangeInserted(positionStart, itemCount);
-                int restaurantCount = mFirebaseAdapter.getItemCount();
-                int lastVisiblePosition = mLinearLayoutManager.findLastVisibleItemPosition();
-                if(lastVisiblePosition == -1 || (positionStart >= (restaurantCount -1) && lastVisiblePosition == (positionStart -1))){
-                    mRestaurantRecyclerView.scrollToPosition(positionStart);
-                }
-            }
-        });
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        //recyclerView.setNestedScrollingEnabled(false);
 
-        mRestaurantRecyclerView.setLayoutManager(mLinearLayoutManager);
-        mRestaurantRecyclerView.setAdapter(mFirebaseAdapter);
+        layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
 
+        adapter = new RecyclerAdapter_Menu();
+        recyclerView.setAdapter(adapter);
 
     }
 
