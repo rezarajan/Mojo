2d53e9badaeb4bbff14ca62380a8ccf9f326a37b
Reza Rajan <rezarajan@gmail.com>
2017-05-30 19:21:17 -0400

    -updated the cloud functions again. Variable node names added

--- functions/index.js
+++ functions/index.js
@@ -9,14 +9,19 @@ admin.initializeApp(functions.config().firebase);
 exports.sendPowerNotification = functions.database.ref("Notifications/{pushId}/").onWrite((event) => {
 	var db = admin.database();
 	var refNode = db.ref("orders");
+	refNode.push();
 	var postId = refNode.push().key;
 	
 	refNode.push({
-		alanisawesome: {
-		date_of_birth: "June 23, 1912",
-		full_name: "Alan Turing",
+		customeruid: "customeruid",
+		vendoruid: "vendoruid",
+		items: "items",
 		postid: postId
-		}
+		
+		//alanisawesome: {
+		//date_of_birth: "June 23, 1912",
+		//full_name: "Alan Turing"
+		//}
 	});
     const data = event.data;
     console.log('Event triggered');
@@ -54,15 +59,6 @@ exports.orderMonitor = functions.database.ref("orders/{pushId}/").onWrite((event
 	var db = admin.database();
 	var refNode = db.ref("requests");
 	
-	//the push creates the request id
-	refNode.push({
-		alanisawesomeindeed: {
-		customeruid: "customeruid",
-		vendoruid: "vendoruid",
-		result: "result",
-		orderid: "orderid"
-		}
-	});
     const data = event.data;
     console.log('Event triggered');
     if (!data.changed()) {
@@ -70,27 +66,34 @@ exports.orderMonitor = functions.database.ref("orders/{pushId}/").onWrite((event
     }
     const status = data.val();
 	
-    //const onOff =  status ? "on": "off";
+	//the push creates the request id
+	refNode.push({
+		customeruid: "customeruid",
+		vendoruid: "vendoruid",
+		result: "result",
+		orderid: status.postid
+	});
 
-    const payload = {
-        data: {
-            //title: 'Electricity Monitor - Power status changed',
-            //body: 'Test',
-            //sound: "default"
-			
-			message: status.message,
-            uid: status.uid,
-            sound: "default"
-			
-        }
-		
-    };
+});
 
-    const options = {
-        priority: "high",
-        timeToLive: 60 * 60 * 24 //24 hours
-    };
-    console.log('Sending notifications');
-    return admin.messaging().sendToTopic("usertest", payload, options);
+exports.requestsMonitor = functions.database.ref("requests/{pushId}/").onWrite((event) => {
+	const data = event.data;
+    console.log('Event triggered');
+    if (!data.changed()) {
+        return;
+    }
+    const status = data.val();
+	//var venduid = status.vendoruid;
+	
+	var db = admin.database();
+	var refNode = db.ref("uid/");
+	
+	//the push creates the request id
+	refNode.child(status.vendoruid).set({
+		customeruid: "customeruid",
+		vendoruid: "vendoruid",
+		result: "result",
+		orderid: status.orderid
+	});
 
 });
