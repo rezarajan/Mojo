8fd040cb7c88ef073a93daceecff87158ff92fa3
rezarajan <rezarajan@gmail.com>
2017-06-17 16:43:05 -0400

    -fixed the bug where the app only shows the orders from Starbucks and crashes. It does not crash anymore, and it gets the correct orders, but the listadapter does not display them for some reason

---
+++ app/src/main/java/bluefirelabs/mojo/menu/MyCallback.java
@@ -0,0 +1,8 @@
+package bluefirelabs.mojo.menu;
+
+/**
+ * Created by rezarajan on 13/06/2017.
+ */ // The callback interface
+interface MyCallback {
+    void callbackCall(String restaurant);
+}
--- app/src/main/java/bluefirelabs/mojo/menu/Vendor_Checkout.java
+++ app/src/main/java/bluefirelabs/mojo/menu/Vendor_Checkout.java
@@ -10,6 +10,8 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -37,6 +39,10 @@ public class Vendor_Checkout extends AppCompatActivity{
     private TextView noitems;
     private String orderId;
     Set keys, mainkey;
+    public String restaurantName = "";
+    public String RESTAURANT = "";
+    private String UID = "";
+    FirebaseAuth firebaseAuth;
 
 
     @Override
@@ -53,15 +59,6 @@ public class Vendor_Checkout extends AppCompatActivity{
         mListView = (ListView)findViewById(R.id.listview_checkout);
         placeorder = (Button)findViewById(R.id.button_place_order);
         noitems = (TextView)findViewById(R.id.content_available_indicator);
-        /*if(data.getCount() > 0){
-            placeorder.setVisibility(View.VISIBLE);
-            mListView.setVisibility(View.VISIBLE);
-            noitems.setVisibility(View.INVISIBLE);
-        } else {
-            placeorder.setVisibility(View.INVISIBLE);
-            mListView.setVisibility(View.INVISIBLE);
-            noitems.setVisibility(View.VISIBLE);
-        } */
 
         populateListView();
     }
@@ -71,41 +68,67 @@ public class Vendor_Checkout extends AppCompatActivity{
 
         final ArrayList<String> listData = new ArrayList<>();
         listData.clear();
+        //create the list adapter and set the adapter
+        ListAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listData);
+
+        final MyCallback myCallback = new MyCallback() {
+            @Override
+            public void callbackCall(String restaurant) {
+                final DatabaseReference reference = FirebaseDatabase.getInstance().getReference(restaurant);
+                reference.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        Map<String, Object> hopperValues = (Map<String, Object>) dataSnapshot.getValue();
+                        //hopperValues.put("key", dataSnapshot.getKey().toString());
+                        //Log.d("Values", dataSnapshot.getKey().toString());
+                        Log.d("Values from Checkout", dataSnapshot.getValue().toString());
 
 
+                        int total_cost = 0;
+
+                        if (hopperValues != null) {
+                            //Map<String, Object> orderID = (Map<String, Object>) hopperValues.get("items");
+                            //Log.d("orderID", orderID.toString());
+
+                            Map<String, Object> itemValues = (Map<String, Object>) hopperValues.get("items");
+
+                            keys = itemValues.keySet();
+
+                            for (Iterator i = keys.iterator(); i.hasNext(); ) {
+                                String key = (String) i.next();
+                                String value = (String) itemValues.get(key);
+                                //textview.setText(key + " = " + value);
+                                Log.d("itemValues", "Key: " + key + ", Value: " + value);
+                                listData.add(key + "    Amount: " + value);
+                                //itemTitle.setText(key.toString());
+
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        };
 
 
-        final DatabaseReference reference = FirebaseDatabase.getInstance().getReference("uid/Starbucks/requests/" + orderId);
-        reference.addValueEventListener(new ValueEventListener() {
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        UID = user.getUid();
+
+        final DatabaseReference reference_restaurantName = FirebaseDatabase.getInstance().getReference("uid/" + UID + "/info");
+        reference_restaurantName.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 Map<String, Object> hopperValues = (Map<String, Object>) dataSnapshot.getValue();
-                //hopperValues.put("key", dataSnapshot.getKey().toString());
-                //Log.d("Values", dataSnapshot.getKey().toString());
-                Log.d("Values", dataSnapshot.getValue().toString());
-
-
-
-                int total_cost = 0;
-
-                if (hopperValues != null) {
-                    //Map<String, Object> orderID = (Map<String, Object>) hopperValues.get("items");
-                    //Log.d("orderID", orderID.toString());
-
-                    Map<String, Object> itemValues = (Map<String, Object>) hopperValues.get("items");
-
-                    keys = itemValues.keySet();
-
-                    for (Iterator i = keys.iterator(); i.hasNext(); ) {
-                        String key = (String) i.next();
-                        String value = (String) itemValues.get(key);
-                        //textview.setText(key + " = " + value);
-                        Log.d("itemValues", "Key: " + key + ", Value: " + value);
-                        listData.add(key + "    Amount: " + value);
-                        //itemTitle.setText(key.toString());
-
-                    }
-                }
+                //Log.d("Values", dataSnapshot.getValue().toString());
+                restaurantName = (String) hopperValues.get("name"); //this directory only contains one item so it should not be a problem
+                RESTAURANT = "uid/" + restaurantName + "/requests/" + orderId;
+                Log.d("Values from Checkout", RESTAURANT);
+                myCallback.callbackCall(RESTAURANT);
             }
 
             @Override
@@ -113,42 +136,9 @@ public class Vendor_Checkout extends AppCompatActivity{
 
             }
         });
-
-        //get the data and append to a list
-        //Cursor data = myDb.getAllData();
-
-
-        //create the list adapter and set the adapter
-        ListAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listData);
-        mListView.setAdapter(adapter);
+        mListView.setAdapter(adapter);  //TODO: This is not displaying the items for some reason. Fix it
         listData.clear();
 
-
-        //set a listener for item clicks
-        //this leads to the edit menu
-       /* mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String name = parent.getItemAtPosition(position).toString();
-                Log.d(TAG, "onItemClicked: You clicked on: " + name);
-
-                Cursor data = myDb.getItemID(name);
-                int itemID = -1;
-                while(data.moveToNext()){
-                    itemID = data.getInt(0);
-                }
-                if(itemID > 0){
-                    Snackbar.make(view, "The ID is: " + itemID,
-                            Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
-
-                } else {
-                    Snackbar.make(view, "No ID associated with that name",
-                            Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
-                }
-            }
-        }); */
     }
 
 
@@ -160,57 +150,6 @@ public class Vendor_Checkout extends AppCompatActivity{
         mListView = (ListView)findViewById(R.id.listview_checkout);
         placeorder = (Button)findViewById(R.id.button_place_order);
         noitems = (TextView)findViewById(R.id.content_available_indicator);
-
-        //Intent receivedIntent = getIntent();
-
-        //now get the orderID passed as an extra
-        //orderId = receivedIntent.getExtras().getString("ID");
-
-        //Cursor data = myDb.getAllData();
-        /*
-        if(data.getCount() > 0){
-            placeorder.setVisibility(View.VISIBLE);
-            mListView.setVisibility(View.VISIBLE);
-            noitems.setVisibility(View.INVISIBLE);
-        } else {
-            placeorder.setVisibility(View.INVISIBLE);
-            mListView.setVisibility(View.INVISIBLE);
-            noitems.setVisibility(View.VISIBLE);
-        } */
         populateListView();
-        /*
-        placeorder.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d("Button clicked", "Placing order");
-
-                FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-                DatabaseReference reference = FirebaseDatabase.getInstance().getReference("orders");
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-
-
-                Map notification = new HashMap<>();
-                Map itemListing = new HashMap<>();
-
-                notification.put("user_token", FirebaseInstanceId.getInstance().getToken());
-                notification.put("customeruid", user.getUid());
-                notification.put("vendoruid", "Starbucks");
-                //notification.put("postid", pushId);
-
-                //get the data and append to a list
-                Cursor data = myDb.getAllData();
-
-                ArrayList<String> listData = new ArrayList<>();
-                while(data.moveToNext()){
-                    //get the value from the database in column
-                    //then add it to the ArrayList
-                    listData.add(data.getString(2));
-                    itemListing.put(data.getString(2), data.getString(4));    //itemId, quantity
-                }
-
-                //reference.child(pushId).setValue(notification);
-                //reference.child(pushId).child("items").setValue(itemListing);
-            }
-        }); */
     }
 }
