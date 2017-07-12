9c207f3451b6722a5dea60ce0a29e074ab329515
Reza Rajan <rezarajan@gmail.com>
2017-07-12 15:35:08 -0400

    -Now the menu displays the specific type of food -still need to work on FirebaseRecyclerAdapterMenu and Restaurant_Menu to pass the restaurant name for the proper directory.

---
+++ public/index.js
@@ -0,0 +1,33 @@
+// JavaScript Document
+var restaurantname = document.getElementById("restaurantname");
+var itemtype = document.getElementById("itemtype");
+var itemname = document.getElementById("itemname");
+var itemcost = document.getElementById("itemcost");
+
+var submitBtn = document.getElementById("submitBtn");
+
+function submitClick() {
+	//window.alert("Submit");
+	var email = "test@gmail.com";
+	var password = "123456";
+	firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // ...
+});
+	var firebaseRef = firebase.database().ref();
+	
+	var resName = restaurantname.value;
+	var itemType = itemtype.value;
+	var itemName = itemname.value;
+	var itemCost = itemcost.value;
+	firebaseRef.child("menu").child(resName).child(itemType).child("Items").child(itemName).set({
+		Quantity : 100,
+		cost : itemCost,
+		name : itemName
+	});
+	firebaseRef.child("menu").child(resName).child("type").set(itemType);
+	//firebaseRef.child("Test").set(resName);
+	
+}
