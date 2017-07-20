68c2ecf4a624b3090b337ea364260ea74ca2ead4
rezarajan <rezarajan@gmail.com>
2017-07-20 01:50:11 -0400

    -You can now add payment sources to Firebase

--- app/src/main/java/bluefirelabs/mojo/handlers/online/Payments.java
+++ app/src/main/java/bluefirelabs/mojo/handlers/online/Payments.java
@@ -4,12 +4,22 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.stripe.android.Stripe;
+import com.stripe.android.TokenCallback;
 import com.stripe.android.model.Card;
+import com.stripe.android.model.Token;
 import com.stripe.android.view.CardInputWidget;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import bluefirelabs.mojo.R;
 
@@ -22,10 +32,12 @@ public class Payments extends AppCompatActivity {
 
         final CardInputWidget mCardInputWidget = (CardInputWidget) findViewById(R.id.card_input_widget);
 
+        final Map card = new HashMap<>();
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(final View view) {
                 Card cardToSave = mCardInputWidget.getCard();
                 if (cardToSave == null) {
                     //mErrorDialogHandler.showError("Invalid Card Data");
@@ -40,6 +52,31 @@ public class Payments extends AppCompatActivity {
                     } else {
                         Snackbar.make(view, "Card Data Valid", Snackbar.LENGTH_LONG)
                                 .setAction("Action", null).show();
+
+                        Stripe stripe = new Stripe(view.getContext(), "pk_test_MKkfFv08ZZK0p6I2Uk6lVpaY");
+                        stripe.createToken(
+                                cardToSave,
+                                new TokenCallback() {
+                                    public void onSuccess(Token token) {
+                                        // Send token to your server
+                                        Log.d("token", token.getId());
+
+                                        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+                                        FirebaseUser user = firebaseAuth.getCurrentUser();
+                                        String uid = user.getUid();
+                                        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("/stripe_customers/" + uid + "/sources");
+                                        String pushId = reference.push().getKey();     //String
+                                        reference.child(pushId).child("token").setValue(token);
+
+                                    }
+                                    public void onError(Exception error) {
+                                        // Show localized error message
+                                        Snackbar.make(view, error.getLocalizedMessage(), Snackbar.LENGTH_LONG)
+                                                .setAction("Action", null).show();
+                                    }
+                                }
+                        );
+
                     }
                 }
             }
